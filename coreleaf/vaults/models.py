56fb4f596db666acf77a55a49d3db871f96122f9
Milan Bairagi <nmilan626@gmail.com>
2025-12-06 18:52:02 +0545

    feat: add vaults app
    Adds the vaults app to manage user credentials and encryption keys.
    
    This commit initializes the vaults application, including models for storing user items and encryption keys, along with necessary configurations and admin settings.

---
+++ coreleaf/vaults/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from accounts.models import User
+
+
+class Item(models.Model):
+    title = models.CharField(max_length=255, blank=True, null=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=255, blank=False, null=True)
+    password = models.CharField(max_length=255)
+    url = models.URLField(blank=True, null=True)
+    notes = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+
+    def __str__(self):
+        return f"{self.title} ({self.user.username})"
+
+
+class encryptionKey(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    key = models.TextField()
+
+
+    def __str__(self):
+        return f"Encryption Key for {self.user.username}"
