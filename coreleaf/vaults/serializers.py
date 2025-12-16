dc570e7a12c59b77479f13abd275b5721b29c1d2
Milan Bairagi <nmilan626@gmail.com>
2025-12-17 00:36:28 +0545

    feat: add serializers for vault creation and listing

---
+++ coreleaf/vaults/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from .models import Item
+
+
+class VaultCreateSerializer(ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ["id", "title", "user", "username", "password", "url", "notes", "created_at", "updated_at"]
+
+
+class VaultListSerializer(ModelSerializer):
+    class Meta:
+        model = Item
+        fields = ["id", "title", "url", "created_at", "updated_at"]
